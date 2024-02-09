6c8b68bf73fff5ff042c6ef4937b41624e7cae99
Cristian Padilla <cristian.padilla@dipzo.net>
2024-02-09 00:39:54 -0500

    Create login and register with API

--- src/app/modules/auth/components/background/background.component.ts
+++ src/app/modules/auth/components/background/background.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
 })
-export class BackgroundComponent implements OnInit {
+export class BackgroundComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
